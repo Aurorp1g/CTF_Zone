010b902bddfb848132d7b445e3ae632889efc65b
Aurorp1g <Ze-662607015@outlook.com>
2025-05-10 21:14:13 +0800

    更新题目

--- verify.js
+++ verify.js
@@ -11,6 +11,7 @@ function rstr(s) {
 const cA = new Map([
     ['shmily', '3D2691DqTjYSWTadFITTTA'],
     ['hex2Virginia', 'uGKXGwEQ/9awSM0Cen8pnw'],
+    ['eerie_RSA', 'pq7exp3XZ5smpeib7r4dQw'],
 ]);
 function rh(ip) {
     try {
